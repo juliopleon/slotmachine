e601a06903c148c87f999d52a20bd7f26209ffd4
Julio Leon <juliopena143@icloud.com>
2024-12-02 16:47:03 -0700

    take bet function

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
 
 
 def deposit():
@@ -33,8 +35,26 @@ def get_number_of_lines():
     return lines
 
 
+def get_bet():
+    while True:
+        amount = input(
+            "Please enter the amount you'd like to bet on each line. $: ")
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f"Amount must be between ${MIN_BET} - ${MAX_BET}")
+        else:
+            print("Please enter an amount.")
+
+    return amount
+
+
 def main():
     balance = deposit()
+    lines = get_number_of_lines()
+    print(balance, lines)
 
 
 main()
