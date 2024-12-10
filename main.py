496a9c4c919c5be091703b78a6830b4967e80247
Julio Leon <juliopena143@icloud.com>
2024-12-10 16:50:05 -0700

    checking winnings

--- main.py
+++ main.py
@@ -15,6 +15,27 @@ symbol_count = {
     "D": 8
 }
 
+symbol_value = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2
+}
+
+
+def check_winnings(columns, lines, bet, values):
+    winnings = 0
+    for line in range(lines):
+        symbol = columns[0][line]
+        for column in columns:
+            symbol_to_check = column[line]
+            if symbol != symbol_to_check:
+                break
+        else:
+            winnings += values[symbol] * bet
+
+    return winnings
+
 
 def get_slot_machine_spin(rows, cols, symbols):
     all_symbols = []
@@ -22,6 +43,7 @@ def get_slot_machine_spin(rows, cols, symbols):
     for symbol, symbol_count in symbols.items():
         for _ in range(symbol_count):
             all_symbols.append(symbol)
+
     columns = []
     for _ in range(cols):
         column = []
@@ -33,6 +55,20 @@ def get_slot_machine_spin(rows, cols, symbols):
             column.append(value)
 
         columns.append(column)
+
+    return columns
+
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            # if i not equal the last index
+            if i != len(columns) - 1:
+                print(column[row], end=" | ")
+            else:
+                print(column[row], end="")
+        # prints a new line character for each col
+        print()
 
 
 def deposit():
@@ -88,7 +124,7 @@ def main():
     lines = get_number_of_lines()
     while True:
         bet = get_bet()
-        total_bet = bet + lines
+        total_bet = bet * lines
 
         if total_bet > balance:
             print(
@@ -97,6 +133,9 @@ def main():
             break
     print(
         f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}")
+
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
 
 
 main()
